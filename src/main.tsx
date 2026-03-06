3ffe201ad8d91a0cd0765f08766494ac18b126a0
Duclems <duclems@gmail.com>
2026-03-06 01:26:16 +0100

    o

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { App } from "./ui/App";
 import { AuthCallbackPage } from "./ui/pages/AuthCallbackPage/AuthCallbackPage";
-import "./styles.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
